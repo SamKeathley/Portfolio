0a36e07428c3aa2e7a2e604dd7dc3ed16aaa53cb
Sam Keathley <samncollette@gmail.com>
2020-06-16 18:37:30 -0700

    mobile nav completed and formatted about page with assets

--- nav.js
+++ nav.js
@@ -4,16 +4,17 @@ document.write('\
     <nav>\
     <button id="hamburger" class="hamburger"><i class="fas fa-bars"></i></button>\
         <ul id="nav-links" class="nav-links">\
-            <li><a class="about" rel="about" href="/about.html"><i class="fas fa-crow"></i></i></a></li>\
-            <li><a class="skills" rel="skills" href="/skills.html"><i class="fas fa-brain"></i></a></li>\
-            <li><a class="projects" rel="projects" href="/projects.html"><i class="fas fa-terminal"></i></a><li>\
-            <li><a href="mailto: keathleysam@gmail.com"><i class="fas fa-envelope"></i></a></li>\
+            <a rel="home" href="/"><i class="fas fa-home"></i><b>HOME</b></a>\
+            <a rel="about" href="/about.html"><i class="fas fa-crow"></i><b>ABOUT</b></a>\
+            <a rel="skills" href="/skills.html"><i class="fas fa-brain"></i><b>SKILLS</b></a>\
+            <a rel="projects" href="/projects.html"><i class="fas fa-terminal"></i><b>PROJECTS</b></a>\
+            <a rel="contact" href="/contact.html"><i class="fas fa-envelope"></i><b>CONTACT</b></a>\
         </ul>\
    </nav>\
         <ul class="social">\
-            <li><a href="#"><i class="fab fa-github"></i></a></li>\
-            <li><a href="#"><i class="fab fa-linkedin-in"></i></a></li>\
-            <li><a href="#"><i class="fab fa-twitter"></i></a></li>\
+            <li><a href="https://github.com/SamKeathley"><i class="fab fa-github"></i></a></li>\
+            <li><a href="https://www.linkedin.com/in/sam-keathley/"><i class="fab fa-linkedin-in"></i></a>\
+            <li><a href="https://twitter.com/SamKeathley"><i class="fab fa-twitter"></i></a></li>\
             </ul>\
     </div>');
 
@@ -24,16 +25,3 @@ hamburger.addEventListener('click', (e) => {
     e.preventDefault();
     navLinks.classList.toggle('show');
 });
-
-// function(e) {
-//     e.preventDefault();
-//     if (!$(this).hasClass('active')) {
-//       if (app.ui.pageLoad($(this).attr('href'), $(this).attr('rel'))) {
-//         $('#nav_bar nav a.active').removeClass('active');
-//         $(this).addClass('active');
-//         window.history.pushState("", "", $(this).attr('href'));
-//       }
-//     }
-//     $('a.logo').removeClass('active');
-//     return false;
-//   }
