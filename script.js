fc2edeca9a91dc9fce83bf055bfb2ed971038ae8
Sam Keathley <samncollette@gmail.com>
2020-10-14 10:34:30 -0700

    add social icons and links

--- script.js
+++ script.js
@@ -4,8 +4,7 @@ const nav1 = document.getElementById('nav-1');
 const nav2 = document.getElementById('nav-2');
 const nav3 = document.getElementById('nav-3');
 const nav4 = document.getElementById('nav-4');
-const nav5 = document.getElementById('nav-5');
-const navItems = [nav1, nav2, nav3, nav4, nav5];
+const navItems = [nav1, nav2, nav3, nav4];
 
 function navAnimation(direction1, direction2) {
   navItems.forEach((nav, i) => {
